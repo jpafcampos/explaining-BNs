d49552981ddab350856726774396058da3008dc9
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2026-04-19 13:50:50 +0100

    update

--- src/synthetic_experiment_parallel.py
+++ src/synthetic_experiment_parallel.py
@@ -541,15 +541,15 @@ def process_single_file(args):
 def run_targeted_sdp_experiment(bif_directory, output_csv="targeted_sdp_random_bns.csv", n_workers=4):
     bif_files = sorted(glob.glob(os.path.join(bif_directory, "*.bif")))
     
-    H_RATIOS = [0.1, 0.25, 0.5, 0.75, 0.90] # Hidden variable ratios to test
+    H_RATIOS = [0.75, 0.90] # Hidden variable ratios to test
     #H_RATIOS = [0.3]
     DECISION_THRESHOLD = 0.5
-    TARGET_BUCKETS = [0.40, 0.50, 0.70, 0.90, 1.0]
+    TARGET_BUCKETS = [0.90, 1.0]
     #TARGET_BUCKETS = [0.5, 0.9]
-    MCMC_TRIALS = 10
+    MCMC_TRIALS = 2
 
-    SIZES_TO_RUN = [20, 50, 100, 200]
-    DENSITIES_TO_RUN = [2, 6]
+    SIZES_TO_RUN = [20, 50]
+    DENSITIES_TO_RUN = [2]
     #SIZES_TO_RUN = [200]
     # Build args list for each file
     args_list = [

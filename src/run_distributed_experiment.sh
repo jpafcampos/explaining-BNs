d833de758c333ea58f4828a1a40bb5e358b2702f
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2026-04-19 11:49:21 +0100

    update

--- src/run_distributed_experiment.sh
+++ src/run_distributed_experiment.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=sdp__parallel_experiment
 #SBATCH --output=results/logs/parallel_experiment_%j.out
 #SBATCH --error=results/logs/parallel_experiment_%j.err
-#SBATCH --time=15:00:00
+#SBATCH --time=7:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=36
 #SBATCH --mem=128G
 
 source ~/miniforge3/bin/activate bn-medical
@@ -15,4 +15,4 @@ export PYTHONUNBUFFERED=1
 python synthetic_experiment_parallel.py \
     --bif-dir ./generated_bif_files/ \
     --output results/final_results.csv \
-    --n-workers 2
+    --n-workers 36

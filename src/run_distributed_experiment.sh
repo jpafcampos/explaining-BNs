8bac642c25b00e01172317dfe2e1f2873d46366d
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2026-04-19 21:52:28 +0100

    update

--- src/run_distributed_experiment.sh
+++ src/run_distributed_experiment.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=results/logs/parallel_experiment_%j.err
 #SBATCH --time=7:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=64G
 
 source ~/miniforge3/bin/activate bn-medical
@@ -18,4 +18,4 @@ export NUMEXPR_NUM_THREADS=1
 python synthetic_experiment_parallel.py \
     --bif-dir ./generated_bif_files/ \
     --output results/final_results_$SLURM_JOB_ID.csv \
-    --n-workers 4
+    --n-workers 1

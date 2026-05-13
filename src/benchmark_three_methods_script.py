55c2d0a65ffe7056b27ae0cf143b5f2376cdb6bf
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2026-05-13 09:53:59 +0100

    update

--- src/benchmark_three_methods_script.py
+++ src/benchmark_three_methods_script.py
@@ -791,4 +791,4 @@ if __name__ == "__main__":
     
     toy_models = models[:2]
 
-    results = run_3_method_targeted_sdp(output_csv="targeted_sdp_benchmark_all_methods_bucket_06.csv", models_to_run=[alarm_model])
+    results = run_3_method_targeted_sdp(output_csv="targeted_sdp_benchmark_all_methods_bucket_06.csv", models_to_run=models)

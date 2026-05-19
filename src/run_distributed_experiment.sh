a0035d90f7d559e85338398dc0ad9d1ee3345330
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2026-05-19 10:33:33 +0100

    update

--- src/run_distributed_experiment.sh
+++ src/run_distributed_experiment.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=sdp__parallel_experiment
 #SBATCH --output=results/logs/parallel_experiment_%j.out
 #SBATCH --error=results/logs/parallel_experiment_%j.err
-#SBATCH --time=30:00:00
+#SBATCH --time=20:00:00
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=100G

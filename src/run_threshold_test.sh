cc8cc1fb789679d38d1c55abb6112af7ed1bb9dd
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2026-05-27 14:48:34 +0100

    threshold test

---
+++ src/run_threshold_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=threshold_test
+#SBATCH --output=results/logs/threshold_test.out
+#SBATCH --error=results/logs/threshold_test.err
+#SBATCH --time=08:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=140G
+
+source ~/miniforge3/bin/activate bn-medical
+
+export PYTHONUNBUFFERED=1
+
+python threshold_test.py
